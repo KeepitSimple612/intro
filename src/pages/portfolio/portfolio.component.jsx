199dc152e529c7cb92b4aa6e6aa0f2b76a6e49e0
John <john.ce@gmail.com>
2020-05-10 18:03:49 -0500

    change in submit

--- src/pages/portfolio/portfolio.component.jsx
+++ src/pages/portfolio/portfolio.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NavBar from "../../components/navBar/navBar";
 import Footer from "../../components/footer/footer.component";
@@ -6,7 +6,40 @@ import Background from "../../img/background.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./portfolio.styles.css";
 
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
 const Portfolio = () => {
+  const [inputFields, setInputFields] = useState({
+    name: "",
+    subject: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const { name, subject, email, phone, message } = inputFields;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...inputFields }),
+    })
+      .then(() => alert("Success!"))
+      .catch((error) => alert(error));
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInputFields({ ...inputFields, [name]: value });
+  };
+
   return (
     <React.Fragment>
       <div className="portfolio-navbar">
@@ -56,23 +89,42 @@ const Portfolio = () => {
             <div className="col s12 m6 offset-m3">
               <div className="card-panel grey lighten-4 grey-text text-darken-4 z-depth-0">
                 <form
-                  method="POST"
+                  onSubmit={handleSubmit}
                   data-netlify="true"
                   data-netlify-recaptcha="true"
                 >
                   <div className="input-field">
                     <i className="material-icons prefix">account_circle</i>
-                    <input type="text" id="name" />
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      value={name}
+                      onChange={handleChange}
+                    />
                     <label htmlFor="name">Name</label>
                   </div>
                   <div className="input-field">
                     <i className="material-icons prefix">subject</i>
-                    <input type="text" id="subject" />
+                    <input
+                      type="text"
+                      id="subject"
+                      name="subject"
+                      value={subject}
+                      onChange={handleChange}
+                    />
                     <label htmlFor="subject">Subject</label>
                   </div>
                   <div className="input-field">
                     <i className="material-icons prefix">email</i>
-                    <input type="email" className="validate" id="email" />
+                    <input
+                      type="email"
+                      className="validate"
+                      id="email"
+                      name="email"
+                      value={email}
+                      onChange={handleChange}
+                    />
                     <label htmlFor="email">Email</label>
                     <span
                       className="helper-text"
@@ -82,19 +134,27 @@ const Portfolio = () => {
                   </div>
                   <div className="input-field">
                     <i className="material-icons prefix">phone</i>
-                    <input type="text" id="phone" />
+                    <input
+                      type="text"
+                      id="phone"
+                      name="phone"
+                      value={phone}
+                      onChange={handleChange}
+                    />
                     <label htmlFor="phone">Phone</label>
                   </div>
                   <div className="input-field">
                     <i className="material-icons prefix">mode_edit</i>
                     <textarea
                       className="materialize-textarea"
+                      name="message"
+                      value={message}
+                      onChange={handleChange}
                       id="message"
                     ></textarea>
                     <label htmlFor="message">Message</label>
                   </div>
                   <div data-netlify-recaptcha="true"></div>
-
                   <button type="submit" className="btn indigo ">
                     Submit
                   </button>
